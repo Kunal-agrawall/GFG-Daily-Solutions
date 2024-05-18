746ae438bab9ba16b3aafc35dd68aa5b2d952411
kunal-agrawall <kunalgarg081@gmail.com>
2024-05-18 22:59:56 +0530

    practice

---
+++ School type/Sum of Series.py
@@ -0,0 +1,20 @@
+class Solution:
+    def seriesSum(self, n: int) -> int:
+        # Using the formula for the sum of the first N natural numbers
+        return n * (n + 1) // 2
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+
+        n = int(input())
+
+        obj = Solution()
+        res = obj.seriesSum(n)
+
+        print(res)
+
+# } Driver Code Ends
