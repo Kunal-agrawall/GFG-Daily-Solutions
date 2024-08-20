6e1c086938e486a584538650697442534dfc0b4f
Kunal-agrawall <kunalgarg081@gmail.com>
2024-08-21 00:10:54 +0530

    21th Aug, 2024 file

---
+++ Problem of the day/Shortest path in Undirected Graph.java
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int m=sc.nextInt();
+            int[][] edge = new int[m][2];
+            for(int i=0;i<m;i++){
+                edge[i][0]=sc.nextInt();
+                edge[i][1]=sc.nextInt();
+            }
+            int src=sc.nextInt();
+            Solution obj = new Solution();
+            int res[] = obj.shortestPath(edge,n,m,src);
+            for(int i=0;i<n;i++){
+                System.out.print(res[i]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+class Solution {
+    public int[] shortestPath(int[][] edges, int n, int m, int src) {
+        // Create an adjacency list for the graph
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        
+        // Populate the adjacency list with edges
+        for (int i = 0; i < m; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            adj.get(u).add(v);
+            adj.get(v).add(u); // Undirected graph
+        }
+        
+        // Distance array to store shortest path from src
+        int[] dist = new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+        
+        // BFS queue
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(src);
+        
+        // Perform BFS
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            for (int neighbor : adj.get(node)) {
+                if (dist[node] + 1 < dist[neighbor]) {
+                    dist[neighbor] = dist[node] + 1;
+                    queue.add(neighbor);
+                }
+            }
+        }
+        
+        // Mark unreachable nodes with -1
+        for (int i = 0; i < n; i++) {
+            if (dist[i] == Integer.MAX_VALUE) {
+                dist[i] = -1;
+            }
+        }
+        
+        return dist;
+    }
+}
