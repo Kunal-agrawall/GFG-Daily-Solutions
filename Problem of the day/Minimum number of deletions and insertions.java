4c231ae2ec220c76f7f27c9fa1df9ecca5199d52
Kunal-agrawall <kunalgarg081@gmail.com>
2024-09-03 19:31:12 +0530

    3rd Sep, 2024 file

---
+++ Problem of the day/Minimum number of deletions and insertions.java
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+//Initial Template for Java
+import java.io.*;
+import java.util.*;
+class GfG
+{
+    public static void main(String args[])
+        {
+            Scanner sc = new Scanner(System.in);
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    String s1 = sc.next();
+                    String s2 = sc.next();
+                    Solution ob = new Solution();
+                    System.out.println(ob.minOperations(s1,s2));
+                }
+        }
+}    
+// } Driver Code Ends
+
+class Solution {
+    public int minOperations(String str1, String str2) {
+        int m = str1.length();
+        int n = str2.length();
+
+        // Create a 2D DP array
+        int[][] dp = new int[m + 1][n + 1];
+
+        // Fill the DP table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        // Length of the longest common subsequence
+        int lcsLength = dp[m][n];
+
+        // Calculate minimum number of operations
+        int insertions = n - lcsLength;
+        int deletions = m - lcsLength;
+
+        return insertions + deletions;
+    }
+}
