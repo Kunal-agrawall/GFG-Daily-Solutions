5e053ce7fc49d73a4ec76c6f49c838e82aafd200
Kunal-agrawall <kunalgarg081@gmail.com>
2024-09-25 00:30:09 +0530

    25th Sept, 2024 file

---
+++ Problem of the day/Palindrome Linked List.py
@@ -0,0 +1,75 @@
+class Solution:
+    def isPalindrome(self, head):
+        # Step 1: Find the middle of the linked list
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # Step 2: Reverse the second half of the list
+        prev = None
+        while slow:
+            next_node = slow.next
+            slow.next = prev
+            prev = slow
+            slow = next_node
+        
+        # Step 3: Compare the first half and the reversed second half
+        left, right = head, prev
+        while right:  # Compare till the end of reversed second half
+            if left.data != right.data:
+                return False
+            left = left.next
+            right = right.next
+        
+        return True
+
+
+#{ 
+ # Driver Code Starts
+#main
+
+
+class Node:
+    # Constructor to initialize the node object
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    # Function to initialize head
+    def __init__(self):
+        self.head = None
+
+    # Function to insert a new node at the beginning
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    # Utility function to prit the linked LinkedList
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print(temp.data, end=" ")
+            temp = temp.next
+        print("")
+
+
+if __name__ == '__main__':
+    t = int(input())
+    while (t > 0):
+        llist = LinkedList()
+        values = input().strip().split()
+        for i in reversed(values):
+            llist.push(i)
+        flag = Solution().isPalindrome(llist.head)
+        if flag:
+            print("true")
+        else:
+            print("false")
+        t -= 1
+# Contributed by: Harshit Sidhwa
+
+# } Driver Code Ends
