521a4852f8dabf081b7e6805843fea7b5011d587
kunal-agrawall <kunalgarg081@gmail.com>
2025-01-20 21:10:21 +0530

    Jan 20, 2025 file

---
+++ Problem of the day/Merge two sorted linked lists.py
@@ -0,0 +1,79 @@
+class Solution:
+    def sortedMerge(self, head1, head2):
+        # Initialize a dummy node to act as the head of the merged list
+        dummy = Node(0)
+        tail = dummy
+        
+        # Merge the two lists
+        while head1 and head2:
+            if head1.data <= head2.data:
+                tail.next = head1
+                head1 = head1.next
+            else:
+                tail.next = head2
+                head2 = head2.next
+            tail = tail.next
+        
+        # Append the remaining nodes, if any
+        if head1:
+            tail.next = head1
+        if head2:
+            tail.next = head2
+        
+        # Return the head of the merged list
+        return dummy.next
+
+
+#{ 
+ # Driver Code Starts
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def print_list(head):
+    temp = head
+    while temp:
+        print(temp.data, end=' ')
+        temp = temp.next
+    print()
+    print("~")
+
+
+def insert_sorted(head, data):
+    new_node = Node(data)
+    if not head or head.data >= data:
+        new_node.next = head
+        return new_node
+
+    current = head
+    while current.next and current.next.data < data:
+        current = current.next
+
+    new_node.next = current.next
+    current.next = new_node
+
+    return head
+
+
+if __name__ == "__main__":
+    T = int(input())
+    for _ in range(T):
+        input1 = list(map(int, input().split()))
+        input2 = list(map(int, input().split()))
+
+        head1 = None
+        for item in input1:
+            head1 = insert_sorted(head1, item)
+
+        head2 = None
+        for item in input2:
+            head2 = insert_sorted(head2, item)
+
+        obj = Solution()
+        merged_head = obj.sortedMerge(head1, head2)
+        print_list(merged_head)
+
+# } Driver Code Ends
