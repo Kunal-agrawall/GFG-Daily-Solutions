78636b274de3f06be5b312244ae58391ec57374b
kunal-agrawall <kunalgarg081@gmail.com>
2025-02-21 00:12:54 +0530

    Feb 21, 2025 file

---
+++ Problem of the day/Feb-2025/Parenthesis Checker.py
@@ -0,0 +1,51 @@
+class Solution:
+    def isBalanced(self, s: str) -> bool:
+        stack = []
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        
+        for char in s:
+            if char in bracket_map.values():  # if it's an opening bracket
+                stack.append(char)
+            elif char in bracket_map.keys():  # if it's a closing bracket
+                if not stack or stack.pop() != bracket_map[char]:
+                    return False
+            else:
+                return False  # unexpected character (though constraints rule this out)
+        
+        return not stack  # valid if stack is empty at the end
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+#Contributed by : Nagendra Jha
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+
+@atexit.register
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        s = str(input())
+        obj = Solution()
+        if obj.isBalanced(s):
+            print("true")
+        else:
+            print("false")
+        print("~")
+
+# } Driver Code Ends
