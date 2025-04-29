b52736fb29617dd8932ce32eeb8c9e2b456479b7
kunal-agrawall <kunalgarg081@gmail.com>
2025-04-29 19:24:21 +0530

    Apr 29, 2025 file

---
+++ Problem of the day/Apr-2025/Sort a linked list of 0s, 1s and 2s.py
@@ -0,0 +1,52 @@
+class Solution:
+    def segregate(self, head):
+        count = [0, 0, 0]
+        curr = head
+        while curr:
+            count[curr.data] += 1
+            curr = curr.next
+        curr = head
+        for i in range(3):
+            while count[i]:
+                curr.data = i
+                curr = curr.next
+                count[i] -= 1
+        return head
+    
+
+
+#{ 
+ # Driver Code Starts
+# Initial Template for Python 3
+
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+def printList(node):
+    while node:
+        print(node.data, end=" ")
+        node = node.next
+    print()
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        arr = list(map(int, input().strip().split()))
+        head = None
+        if arr:
+            head = Node(arr[0])
+            tail = head
+            for value in arr[1:]:
+                tail.next = Node(value)
+                tail = tail.next
+
+        printList(Solution().segregate(head))
+        print("~")
+
+# } Driver Code Ends
